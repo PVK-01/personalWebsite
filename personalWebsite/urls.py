756aaad1849940c54d1e7592ea7f37a4dcdc5ef3
PVK-01 <vamsi010199@gmail.com>
2022-01-13 23:11:45 +0530

    removed unnecessary code

--- personalWebsite/urls.py
+++ personalWebsite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from personalApp import views
 from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +28,4 @@ urlpatterns = [
     path('PetroleumEngineering', views.PetroleumEngineering),
     path('Skills', views.Skills),
     path('WorkExperience', views.WorkExperience),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
